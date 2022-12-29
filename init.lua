b5ae970029e295725c4dc85a7ad934a90d020deb
Hongbo He <hhbcarl@gmail.com>
2022-12-29 23:59:27 +0800

    Add commit-id for packer

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
+  -- commit dac4088c70f4337c6c40d1a2751266a324765797
   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
   vim.cmd [[packadd packer.nvim]]
 end
@@ -19,10 +20,8 @@ require('packer').startup(function(use)
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
-
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
     },
@@ -52,21 +51,11 @@ require('packer').startup(function(use)
   use 'rhysd/vim-gfm-syntax'
   use "ellisonleao/gruvbox.nvim"
 
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
-
   if is_bootstrap then
     require('packer').sync()
   end
 end)
 
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
 if is_bootstrap then
   print '=================================='
   print '    Plugins are being installed'
