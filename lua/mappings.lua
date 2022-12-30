13dc9add603fae98fc31e599d4032b95f4c010de
Hongbo He <hhbcarl@gmail.com>
2022-12-31 01:37:13 +0800

    Add <C-p> to telescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -48,3 +48,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Telescope
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
