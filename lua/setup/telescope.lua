d587423fa019ca4d49e74cbed74d8f8f0499b44a
Hongbo He <hongbo.he@veeva.com>
2023-01-05 18:20:40 +0800

    Telescope add ignore files

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,3 +1,8 @@
 -- See `:help telescope` and `:help telescope.setup()`
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
+require('telescope').setup {
+    defaults = {
+        file_ignore_patterns = {"*.pyc", '__pycache__'},
+    }
+}
