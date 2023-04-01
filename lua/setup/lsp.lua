0c101c4c12257ea717b9efc9deaa1e5a1ca9cc33
Hongbo He <hhbcarl@gmail.com>
2023-04-01 21:52:40 +0800

    Append instead of prepend of mason path

--- lua/setup/lsp.lua
+++ lua/setup/lsp.lua
@@ -64,7 +64,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Setup mason so it can manage external tooling
-require('mason').setup()
+require('mason').setup({PATH="append"})
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
