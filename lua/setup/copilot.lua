dd80d3b643bdc754094d11765ba1390aecca2210
Hongbo He <hongbo.he@veeva.com>
2024-03-28 16:05:07 +0800

    Switch on/off copilot by env var

--- lua/setup/copilot.lua
+++ lua/setup/copilot.lua
@@ -1,18 +1,26 @@
 -- this function should be called before loading the plugin
 local function init()
   -- Only enable copilot for these filetypes
-  vim.g.copilot_filetypes = {
-    ['*'] = false,
-    python = true,
-    bash = true,
-    zsh = true,
-    sh = true,
-    lua = true,
-    javascript = true,
-    vim = true,
-    gitcommit = true,
-    markdown = true,
-  }
+  if vim.env.VIM_COPILOT == '0' then
+    vim.g.copilot_filetypes = {
+      ['*'] = false
+    }
+  else
+    vim.g.copilot_filetypes = {
+      ['*'] = false,
+      python = true,
+      bash = true,
+      zsh = true,
+      sh = true,
+      lua = true,
+      javascript = true,
+      vim = true,
+      gitcommit = true,
+      markdown = true,
+      toml = true,
+      yaml = true,
+    }
+  end
   -- Use <C-J> to accept the suggestion
   vim.g.copilot_no_tab_map = true
   vim.cmd([[
