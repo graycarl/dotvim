19439d662206824b1cb0288e7984fa6c3d59e6b8
Hongbo He <hongbo.he@veeva.com>
2024-04-25 17:40:51 +0800

    Upgrade lazy.vim

--- init.lua
+++ init.lua
@@ -14,7 +14,8 @@ vim.g.maplocalleader = ' '
 -- Setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath})
+  vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable",
+                 "https://github.com/folke/lazy.nvim.git", lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup('plugins', {
