3bee268423a84a6d98a583175d311b6b9c81584b
Hongbo He <hongbo.he@veeva.com>
2024-04-25 17:03:46 +0800

    Remove neorg

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -122,31 +122,6 @@ return {
     config = require('setup.toggleterm').config,
   },
 
-  {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-        },
-      }
-
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
-    end,
-  },
-
   -- local plugins
   { dir = vim.fn.stdpath('config') .. '/pack/my/start/encrypt' },
   { dir = vim.fn.stdpath('config') .. '/pack/my/start/vault' },
