52d96b729fb5115d084369cf634de8e10515c3ef
Hongbo He <hongbo.he@veeva.com>
2024-05-14 10:11:11 +0800

    Improve colorswitch

--- plugin/colorswitch.lua
+++ plugin/colorswitch.lua
@@ -9,6 +9,12 @@ function SwitchColor(direction)
     current = vim.g.colors_name
   end
   local colors = vim.fn.getcompletion('', 'color')
+  -- Remove colors with name contains `-`
+  for i = #colors, 1, -1 do
+    if string.match(colors[i], '-') then
+      table.remove(colors, i)
+    end
+  end
   local idx = -1
   for i, color in ipairs(colors) do
     if color == current then
@@ -19,16 +25,30 @@ function SwitchColor(direction)
   if idx == -1 then
     return
   end
-  local new_idx = idx + direction
-  if new_idx < 1 then
-    new_idx = #colors
-  elseif new_idx > #colors then
-    new_idx = 1
+  -- Switch background if current colors name do not contains `-`
+  if vim.o.background == 'light' and direction == 1 then
+    vim.o.background = 'dark'
+    print('Switched to [' .. idx .. '] ' .. current .. ' with dark mode')
+  elseif vim.o.background == 'dark' and direction == -1 then
+    vim.o.background = 'light'
+    print('Switched to [' .. idx .. '] ' .. current .. ' with light mode')
+  else
+    local new_idx = idx + direction
+    if new_idx < 1 then
+      new_idx = #colors
+    elseif new_idx > #colors then
+      new_idx = 1
+    end
+    vim.cmd('colorscheme ' .. colors[new_idx])
+    current = colors[new_idx]
+    if direction == -1 then
+      vim.o.background = 'dark'
+      print('Switched to [' .. new_idx .. '] ' .. colors[new_idx] .. ' with dark mode')
+    else
+      vim.o.background = 'light'
+      print('Switched to [' .. new_idx .. '] ' .. colors[new_idx] .. ' with light mode')
+    end
   end
-  vim.cmd('colorscheme ' .. colors[new_idx])
-  current = colors[new_idx]
-  -- Print the new colorscheme
-  print('Switched to [' .. new_idx .. '] ' .. colors[new_idx])
 end
 
 -- Create commands to switch colorscheme
