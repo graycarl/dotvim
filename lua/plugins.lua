e2ea1907427643565d7ff396e7a3e41241ff5e57
Hongbo He <graycarl@qq.com>
2024-07-30 23:35:08 +0800

    Upgrade gruvbox.nvim to 2.0

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,7 +51,7 @@ return {
   },
 
   -- Color schemes
-  "ellisonleao/gruvbox.nvim",
+  {"ellisonleao/gruvbox.nvim", version = '2.0'},
   {'sainnhe/everforest', version = 'v0.3'},
   'navarasu/onedark.nvim',
   'rmehri01/onenord.nvim',
