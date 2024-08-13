87f278a51d1dec165b51b29bf501202bbb404b2e
Hongbo He <hongbo.he@veeva.com>
2024-08-13 14:49:35 +0800

    Fix for vim-test upgrading

--- lua/setup/vim-test.lua
+++ lua/setup/vim-test.lua
@@ -1,6 +1,6 @@
 local function init()
   -- strategy settings: <https://github.com/vim-test/vim-test?tab=readme-ov-file#strategies>
-  vim.g["test#strategy"] = "make" -- or "toggleterm"
+  vim.g["test#strategy"] = "neovim" -- or "toggleterm"
 end
 
 return {
