771bdd97b88eff84247159fcffd5573e10666788
Hongbo He <hongbo.he@veeva.com>
2024-09-02 14:40:05 +0800

    vim-test strategy update

--- lua/setup/vim-test.lua
+++ lua/setup/vim-test.lua
@@ -1,6 +1,6 @@
 local function init()
   -- strategy settings: <https://github.com/vim-test/vim-test?tab=readme-ov-file#strategies>
-  vim.g["test#strategy"] = "neovim" -- or "toggleterm"
+  -- vim.g["test#strategy"] = "toggleterm" -- or "toggleterm"
 end
 
 return {
