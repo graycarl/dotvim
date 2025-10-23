9cabe8c029e9657d18396c9490777fbf866b03d6
Hongbo He <hhbcarl@gmail.com>
2025-10-24 00:06:43 +0800

    Fix gemini config typo

--- lua/setup/gemini.lua
+++ lua/setup/gemini.lua
@@ -1,13 +1,16 @@
 -- See: <https://github.com/kiddos/gemini.nvim>
 local opts = {
   model_config = {
-    -- model_id = "gemini-2.5-flash",
-    model_id = "gemini-pro",
+    model_id = "gemini-2.5-flash",
+    -- model_id = "gemini-2.5-pro",
   },
   completion = {
     enabled = true,
     blacklist_filetypes = { 'vault' },
     blacklist_filenames = { '.env', '.enc' },
     completion_delay = 500,
+    insert_result_key = '<C-J>',
   },
 }
+
+return opts
